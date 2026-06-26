e46e5a755561d224c89fef55db83064bad814b09
xingyuan <songqixuan@qq.com>
2026-06-27 01:42:00 +0800

    revert: remove basePath, use direct port access

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/zhijiao',
+  // basePath not needed — access via http://47.107.31.231:3001
 };
 
 module.exports = nextConfig;
